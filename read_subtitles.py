dfbb98da183871ae3d0b84f24e69d4b6fe28193e
Bhairav Mehta <bhairavmehta95@gmail.com>
2016-07-29 22:05:30 -0500

    added multithreading, fixed subtitle parsing to match only the correct questions

--- read_subtitles.py
+++ read_subtitles.py
@@ -1,3 +1,5 @@
+import re
+
 def process_ts(timestamp):
 	# 1 hr = 3600000 ms
 	# 1 min = 60000 ms
@@ -11,7 +13,6 @@ def process_ts(timestamp):
 	ms += 1000 * int(last[0])
 	ms += int(last[1])
 
-	print timestamp, ms
 	return ms
 
 def read_subtitles(file):
@@ -19,29 +20,48 @@ def read_subtitles(file):
 	srt = open(file, 'r')
 	lines = srt.readlines()
 	i = 0
-	print i, len(lines)
+	#print i, len(lines)
 	timestamps = []
 	count = 1
 	lines = iter(lines)
+	dialogues = []
 
+	old_start_timestamp = 0
+	added_question = False
 	# gets timestamp ending times for lines with questions
 	for line in lines:
 		target = str(count) + '\n'
 		if (line == target):
 			count += 1
 			timestamp = lines.next()
+			start_timestamp = timestamp.split()[0]
+			start_timestamp = process_ts(start_timestamp)
+			if added_question and not abs(start_timestamp - old_start_timestamp) >= 4000:
+				timestamps.pop()
+				dialogues.pop()
+
 			dialogue = ''
 			read = lines.next()
 			while read != '\n':
 				dialogue += read
 				read = lines.next()
-			if dialogue.find('?') != -1:
+
+			last_dash = dialogue.rfind('-')
+			if last_dash == dialogue.find('-'):
+				last_dash = dialogue.find('?')
+
+			if dialogue.find('?') != -1 and dialogue.find('?') <= last_dash:
+				added_question = True
+				old_start_timestamp = timestamp.split()[0]
+				old_start_timestamp = process_ts(old_start_timestamp)
 				timestamp = timestamp.split()[2]
 				# returns in milliseconds
 				timestamp = process_ts(timestamp)
 				timestamps.append(timestamp)
+				dialogues.append(dialogue)
 
-	return timestamps
+	print dialogues
+	return {'timestamps' : timestamps, 'dialogues' : dialogues} 
 
 
 if __name__ == '__main__':
--- video.py
+++ video.py
@@ -8,8 +8,55 @@ import functools
 import time
 from read_subtitles import read_subtitles
 
+import speech_recognition as sr
+import threading
 
-def play_vlc_video(movie, end_times):
+
+
+class speech_thread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        text = speech_query()
+        print text
+
+def speech_query():
+    r = sr.Recognizer()
+    m = sr.Microphone()
+
+    try:
+        print("A moment of silence, please...")
+        # TO DO: Fix ambient noise?
+
+        #with m as source: r.adjust_for_ambient_noise(source)
+        #print("Set minimum energy threshold to {}".format(r.energy_threshold))
+
+        print("Say something!")
+        with m as source: audio = r.listen(source)
+        print("Got it! Now to recognize it...")
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_google(audio)
+
+            # we need some special handling here to correctly print unicode characters to standard output
+            if str is bytes: # this version of Python uses bytes for strings (Python 2)
+                print(u"You said {}".format(value).encode("utf-8"))
+            else: # this version of Python uses unicode for strings (Python 3+)
+                print("You said {}".format(value))
+
+            return value
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+    except KeyboardInterrupt:
+        pass
+
+
+def play_vlc_video(movie, end_times, dialogues):
     try:
         from msvcrt import getch
     except ImportError:
@@ -46,27 +93,41 @@ def play_vlc_video(movie, end_times):
         'q': quit_app,
         }
 
-    while True:
-        fd = sys.stdin.fileno()
+    triggered = False
+    fd = sys.stdin.fileno()
+
+    # To Test
+    #end_times[0] = 3000
+    i = 0
+
+    # TO DO: Check Thresholds, as well as audio timeout
+    while True and not triggered:
         elapsed = player.get_time()
-        print elapsed, end_times[0]
+        if elapsed - end_times[i] > 1000:
+            i += 1
+            continue
+        if end_times[i] - elapsed < 2000 and not triggered:
+            print "Question at: ", end_times[i], 'is', dialogues[i]
+            thread1 = speech_thread(1, "Thread-1", 1)
+            thread1.start()
 
-        # TO DO: check thresholds
-        if abs(elapsed - end_times[0]) < 1000:
-            player.pause()
+            while thread1.is_alive():
+                elapsed = player.get_time()
+                if elapsed - end_times[i] > 4000:
+                    i += 1
+                    break
 
-            # call other function here
-
-            ### do processing here 
-            ###
-            time.sleep(3)
-            return
-
-
+            print "Resuming"
+            triggered = False
+            i += 1
+            #player.play()
+                
 
 if __name__ == '__main__':
     movie = '../dora.mp4'
     subtitles = 'dora.srt'
-    end_times = read_subtitles(subtitles)
+    read_sub_return = read_subtitles(subtitles)
+    end_times = read_sub_return['timestamps']
+    dialogues = read_sub_return['dialogues']
     #end_times = [10699]
-    play_vlc_video(movie, end_times)
+    play_vlc_video(movie, end_times, dialogues)
